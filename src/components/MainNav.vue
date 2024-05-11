2f63543fb05cae7765d58d1f5a759ad39da1497d
Pakapol Tanachaisith <pakapol.tanachaisith@gmail.com>
2024-05-11 14:30:02 +0700

    Fix styling proble in the main nav

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,27 +2,27 @@
   <nav>
     <ul class="sm:flex hidden gap-4 sm:gap-6 text-grayish-blue-500">
       <li
-        class="py-8 hover:text-primary-500 hover:border-b-2 border-primary-500"
+        class="py-8 hover:text-primary-500 border-b-2 border-b-transparent hover:border-primary-500"
       >
         <a href="/">Collections</a>
       </li>
       <li
-        class="py-8 hover:text-primary-500 hover:border-b-2 border-primary-500"
+        class="py-8 hover:text-primary-500 border-b-2 border-b-transparent hover:border-primary-500"
       >
         <a href="/">Men</a>
       </li>
       <li
-        class="py-8 hover:text-primary-500 hover:border-b-2 border-primary-500"
+        class="py-8 hover:text-primary-500 border-b-2 border-b-transparent hover:border-primary-500"
       >
         <a href="/">Women</a>
       </li>
       <li
-        class="py-8 hover:text-primary-500 hover:border-b-2 border-primary-500"
+        class="py-8 hover:text-primary-500 border-b-2 border-b-transparent hover:border-primary-500"
       >
         <a href="/">About</a>
       </li>
       <li
-        class="py-8 hover:text-primary-500 hover:border-b-2 border-primary-500"
+        class="py-8 hover:text-primary-500 border-b-2 border-b-transparent hover:border-primary-500"
       >
         <a href="/">Contacts</a>
       </li>
