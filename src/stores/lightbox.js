d59300026bcf49040afc8f334f80b16f49668f05
Pakapol Tanachaisith <pakapol.tanachaisith@gmail.com>
2024-05-11 15:05:59 +0700

    Fix bug in lightbox store

--- src/stores/lightbox.js
+++ src/stores/lightbox.js
@@ -1,4 +1,15 @@
 import { ref } from "vue";
 import { defineStore } from "pinia";
 
-export const useLightboxStore = defineStore("lightbox", () => {});
+export const useLightboxStore = defineStore("lightbox", () => {
+  const isOpen = ref(false);
+
+  const onOpen = () => (isOpen.value = true);
+  const onClose = () => (isOpen.value = false);
+
+  return {
+    isOpen,
+    onClose,
+    onOpen,
+  };
+});
